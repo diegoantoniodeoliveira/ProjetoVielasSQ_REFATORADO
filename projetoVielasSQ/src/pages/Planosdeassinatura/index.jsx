371d5ccd8e5804543657bf4f24d75269b2eee7d0
diegoantoniodeoliveira <diegoantoniodeoliveira@hotmail.com>
2024-06-28 10:17:54 -0300

    planoseassinatura

---
+++ projetoVielasSQ/src/pages/Planosdeassinatura/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './styles.module.css';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+
+
+export  const PlanosDeAssinatura = () => {
+  return ( 
+   <>
+      <Header />
+
+
+
+
+
+
+
+      <Footer />
+    </>
+  );
+
+}
+  
+  export default PlanosDeAssinatura;
+  
--- projetoVielasSQ/src/pages/home/index.jsx
+++ projetoVielasSQ/src/pages/home/index.jsx
@@ -106,3 +106,4 @@ export const Home = () => (
 );
 
 export default Home;
+
